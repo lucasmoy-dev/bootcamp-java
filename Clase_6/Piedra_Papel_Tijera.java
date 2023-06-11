ab15620cac713f4c2ada6827077d02db0434d077
esassu1977 <simedservicios@gmail.com>
2023-06-11 00:34:34 -0600

    Actividades de la clase 6

--- Clase_6/Piedra_Papel_Tijera.java
+++ Clase_6/Piedra_Papel_Tijera.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 
 
-/*Escribe un programa que permita al usuario jugar el juego clásico de "piedra, papel, tijeras" contra la
+/* Escribe un programa que permita al usuario jugar el juego clásico de "piedra, papel, tijeras" contra la
 computadora. El programa debe solicitar al usuario que ingrese su elección (piedra, papel o tijeras) y
 luego generar una elección aleatoria para la computadora. Después de eso, el programa debe determinar el
 ganador según las reglas del juego y mostrar el resultado.
